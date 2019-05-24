9fd6433ab1976a885838614ecc88b060f094f614
Mateusz Piwek <Mateusz@teaFS.org>
2019-05-25 00:05:38 +0200

    Waiting for mysql command tested

--- files/entry.sh
+++ files/entry.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+# probe for MariaDB connection over TCP
+
+# If MariaDB is linked from other continer, probe network connection before launching server
+# if $MYSQL_HOST is not set, or set as 'localhost', or '127.0.0.1', or '::1' 
+mysql  -u root --wait --connect-timeout=1 --reconnect=TRUE -e '\q'
+
+[ $(grep -w $MYSQL_HOST </etc/hosts 2&>/dev/null | grep -w localhost | wc -l) == 0 ] && wait_for_net_maria;
+
 if [ ! -e "./wp-config.php" ]; then 
 	echo "First run, deploying Wordpress"
 
